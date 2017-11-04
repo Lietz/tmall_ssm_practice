4c8a3dd66bd5c9da6b50f5f3ed71b7bdf4eae6b4
GoLong <dajianglong225@sina.com>
2017-11-04 12:30:27 +0800

    After BatisGenerated Before HouTaiQiTaYeMian

--- src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/how2java/tmall/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.how2java.tmall.service.impl;
 import com.how2java.tmall.mapper.CategoryMapper;
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.pojo.CategoryExample;
 import com.how2java.tmall.service.CategoryService;
 import com.how2java.tmall.util.Page;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,9 @@ public class CategoryServiceImpl  implements CategoryService {
     CategoryMapper categoryMapper;
     @Override
     public List<Category> list(){
-        return categoryMapper.list();
+        CategoryExample example =new CategoryExample();
+        example.setOrderByClause("id desc");
+        return categoryMapper.selectByExample(example);
     };
 
 //    @Override
@@ -22,18 +25,18 @@ public class CategoryServiceImpl  implements CategoryService {
 
     @Override
     public void add(Category category) {
-        categoryMapper.add(category);
+        categoryMapper.insert(category);
     }
     @Override
     public void delete(int id) {
-        categoryMapper.delete(id);
+        categoryMapper.deleteByPrimaryKey(id);
     }
     @Override
     public Category get(int id) {
-        return categoryMapper.get(id);
+        return categoryMapper.selectByPrimaryKey(id);
     }
     @Override
     public void update(Category category) {
-        categoryMapper.update(category);
+        categoryMapper.updateByPrimaryKeySelective(category);
     }
 }
